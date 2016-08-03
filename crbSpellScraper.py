4ac15ed4d53a38426a8bd742f7bb05d7abddc396
Mike <phegan@gmail.com>
2016-08-02 22:22:30 -0400

    Changed to iterating over the paragraphs instead of using specific indexes, since this is more accurate. Added support for parsing spell components, may still have a bug where a material component shows up in the list.

--- crbSpellScraper.py
+++ crbSpellScraper.py
@@ -66,17 +66,47 @@ def scrape_spell_page(soup):
     spell = {}
     body = soup.find_all('body')
     paragraphs = body[0].find_all('p')
-    spell["school"], spell["levels"] = parse_school_and_levels(paragraphs[1])
-    spell["action"] = parse_action(paragraphs[2])
-    spell["components"] = parse_components(paragraphs[3])
 
-    # print spell
+    for paragraph in paragraphs:
+        if "School" in paragraph.text:
+            spell["school"], spell["levels"] = parse_school_and_levels(paragraph)
+        elif "Casting Time" in paragraph.text:
+            spell["action"] = parse_action(paragraph)
+        elif "Components" in paragraph.text:
+            spell["components"] = parse_components(paragraph)
+
+    print spell
 
 def parse_action(paragraph):
     return paragraph.text.replace("Casting Time", "")
 
 def parse_components(paragraph):
-    print paragraph.text.replace("Components", "").replace("Component:", "").split(",")
+    final_components = []
+    components = paragraph.text.replace("Components", "").replace("Component:", "").split(",")
+    for component in components:
+        if any(comp in component for comp in ['M', 'F']):
+            parsed_components = parse_component_with_item(component)
+            if '/' in parsed_components[0]:
+                final_components.extend([[x, parsed_components[1]] for x in parsed_components[0].split('/')])
+            else:
+                if len(parsed_components) == 1:
+                    final_components.append(parsed_components[0])
+                else:
+                    final_components.append(parsed_components)
+        else:
+            final_components.append(component)
+
+    return final_components
+
+
+
+def parse_component_with_item(component):
+    components = component.strip().split(" ", 1)
+    if len(components) < 2:
+        return components
+
+    components[1] = components[1].replace('(', "").replace(")", "")
+    return components
 
 def parse_school_and_levels(paragraph):
     parts = paragraph.text.split(";")
